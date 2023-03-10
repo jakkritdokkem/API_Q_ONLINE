1670519f6da13ad7fd2fa177b7caff7f73419c8d
jakkritdokkem <boom0928754237@gmail.com>
2023-03-10 08:54:01 +0700

    FIX BUG

--- routes/BookAppointment.js
+++ routes/BookAppointment.js
@@ -8,30 +8,8 @@ router.post('/createBookAppointment', async (req, res) => {
     console.log('req.body', req.body);
 
     const { openScheduleId, userId, note } = req.body;
-    const code = await gen.genarateCode(mssql);
 
-    console.log("code", code);
-
-    res.status(200).send(code);
-
-    // const query = `SELECT id, id_card, CONCAT(name, ' ', lastname) AS fullname, role from [user] WHERE id_card = '${username}' AND password = '${password}' AND is_used = '1'`;
-
-    // await mssql.sql.query(query, (err, data) => {
-    //   if (data) {
-    //     let query = respon.single(data.recordset);
-    //     if (query.data) {
-    //       res.status(200).send(query);
-    //     } else {
-    //       res.status(200).send(respon.error(200, 'เลขบัตรประจำตัวประชาชน หรือรหัสผ่านไม่ถูกต้อง'));
-    //     }
-    //   } else {
-    //     if (err) {
-    //       res.status(500).send(respon.error(err.originalError.info.number, err.originalError.info.message));
-    //     } else {
-    //       res.status(500).send(respon.error());
-    //     }
-    //   }
-    // });
+    res.status(200).send(req.body);
   } catch (error) {
     console.log(error);
   }
